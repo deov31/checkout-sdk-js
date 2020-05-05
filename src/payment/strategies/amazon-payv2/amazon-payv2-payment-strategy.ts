94f5de3612f198d688b7091e78b391a1102c37b7
Axel Malagon <axel.malagon@bigcommerce.com>
2020-05-05 14:34:21 -0500

    refactor(payments): INT-2119 Changed structure submit payment payload

--- src/payment/strategies/amazon-payv2/amazon-payv2-payment-strategy.ts
+++ src/payment/strategies/amazon-payv2/amazon-payv2-payment-strategy.ts
@@ -6,7 +6,6 @@ import { InvalidArgumentError, MissingDataError, MissingDataErrorType, RequestEr
 import { OrderActionCreator, OrderRequestBody } from '../../../order';
 import { OrderFinalizationNotRequiredError } from '../../../order/errors';
 import { AmazonPayv2PaymentProcessor, AmazonPayv2PayOptions, AmazonPayv2Placement } from '../../../payment/strategies/amazon-payv2';
-import { PaymentArgumentInvalidError } from '../../errors';
 import PaymentActionCreator from '../../payment-action-creator';
 import PaymentMethodActionCreator from '../../payment-method-action-creator';
 import { PaymentInitializeOptions, PaymentRequestOptions } from '../../payment-request-options';
@@ -84,18 +83,16 @@ export default class AmazonPayv2PaymentStrategy implements PaymentStrategy {
         const { paymentToken } = paymentMethod.initializationData;
 
         if (paymentToken) {
-            const { payment, ...order } = payload;
-            const paymentData = payment && payment.paymentData && {nonce:paymentToken};
-
-            if (!payment) {
-                throw new PaymentArgumentInvalidError(['payment']);
-            }
+            const { ...order } = payload;
+            const paymentPayload = {
+                methodId: this._methodId,
+                paymentData: { nonce: paymentToken },
+            };
 
             try {
                 await this._store.dispatch(this._orderActionCreator.submitOrder(order, options));
-                const paymentRequest = await this._store.dispatch(this._paymentActionCreator.submitPayment({...payment, paymentData }));
-
-                return paymentRequest;
+                return this._store.dispatch(this._paymentActionCreator.submitPayment(paymentPayload));
+                
             } catch (error) {
                 if (!(error instanceof RequestError) || !some(error.body.errors, { code: 'three_d_secure_required' })) {
                     return Promise.reject(error);
